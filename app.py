42e7dfd29e1b708987a7d59e53ef138b26bcedfe
Diamond Bishop <bishop@fb.com>
2022-11-03 23:10:28 -0700

    updating to single page splashpage w/ recruiting note

--- app.py
+++ app.py
@@ -31,10 +31,6 @@ def index():
         storeEmail(email)
     return render_template('index.html', email=email)
 
-@app.route("/join")
-def join():
-    return render_template('recruiting.html')
-
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0')
     serve(app, host='0.0.0.0', port=80)
